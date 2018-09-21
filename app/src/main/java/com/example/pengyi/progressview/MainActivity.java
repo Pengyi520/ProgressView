7746ca72bf9dbf88aa18d59ba303875b975fa69a
pengyi14 <pengyi14@wanda.cn>
2018-09-21 23:54:59 +0800

    设置两种效果@

--- app/src/main/java/com/example/pengyi/progressview/MainActivity.java
+++ app/src/main/java/com/example/pengyi/progressview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pengyi.progressview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 /**
@@ -9,16 +10,24 @@ import android.widget.Toast;
  */
 
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ProgressView progressView = findViewById(R.id.progress_view);
-        progressView.setOnFinishListner(new ProgressView.OnFinishListner() {
-            @Override
-            public void onFinish() {
-                Toast.makeText(MainActivity.this,"finish !!!!!",Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ProgressView progressView = findViewById(R.id.progress_view);
+    progressView.setOnClickListener(new ProgressView.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(MainActivity.this, "onClick !!!!!", Toast.LENGTH_SHORT).show();
+      }
+    });
+
+    ProgressView progressView2 = findViewById(R.id.progress_view2);
+    progressView2.setOnFinishListener(new ProgressView.OnFinishListener() {
+      @Override
+      public void onFinish() {
+        Toast.makeText(MainActivity.this, "onfinish !!!!!", Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
 }
--- app/src/main/java/com/example/pengyi/progressview/ProgressView.java
+++ app/src/main/java/com/example/pengyi/progressview/ProgressView.java
@@ -1,5 +1,6 @@
 package com.example.pengyi.progressview;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -18,23 +19,27 @@ import android.view.View;
 public class ProgressView extends View {
   private int bgColor = Color.RED;
   private int startRadius; // 小圆的半径
-  private int endRadius; // 大圆的半径
-  private int diffRadius; // 大圆小圆半径差
-  private int progressWidth; // 进度的宽度
-  private int progressColor = Color.WHITE;
+  private int maxDiffRadius; // 大圆小圆最大半径差
+  private int diffRadius; // 大圆小圆当前半径差
+  private int progressWidth; // 进度条的宽度
+  private int duration = 1000; // 进度条转满一圈的耗时
+  private int progressColor = Color.WHITE; // 进度条颜色
+  private int progressSecondColor; // 进度条背景色
+  private boolean progressEnable = false; // 是否带进度条，false 不画进度条
+
   private int xCenter;
   private int yCenter;
 
   private Paint cirlePaint;
   private Paint progressPaint;
+  private Paint progressSecondPaint;
 
   private boolean isClick = false;
+  private boolean isFinish = false;
   private RectF progressOval;
   private int progress = 0;
-  private boolean isFinish = false;
-  private OnFinishListner onFinishListner;
-  private int rate = 5;
-
+  private OnFinishListener onFinishListener; // 带进度条设置
+  private OnClickListener onClickListener; // 不带进度条设置
 
 
   public ProgressView(Context context) {
@@ -44,10 +49,14 @@ public class ProgressView extends View {
   public ProgressView(Context context, @Nullable AttributeSet attrs) {
     super(context, attrs);
     TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ProgressView);
-    diffRadius = (int) typedArray.getDimension(R.styleable.ProgressView_diff_radius, 0);
+    maxDiffRadius = (int) typedArray.getDimension(R.styleable.ProgressView_diff_radius, 0);
     progressWidth = (int) typedArray.getDimension(R.styleable.ProgressView_progress_width, 0);
+    duration = typedArray.getInt(R.styleable.ProgressView_duration, 1000);
     bgColor = typedArray.getColor(R.styleable.ProgressView_bg_color, Color.RED);
     progressColor = typedArray.getColor(R.styleable.ProgressView_progress_color, Color.WHITE);
+    progressSecondColor =
+        typedArray.getColor(R.styleable.ProgressView_progress_second_color, Color.WHITE);
+    progressEnable = typedArray.getBoolean(R.styleable.ProgressView_progress_enable, false);
     init();
   }
 
@@ -62,6 +71,12 @@ public class ProgressView extends View {
     progressPaint.setColor(progressColor);
     progressPaint.setStyle(Paint.Style.STROKE);
     progressPaint.setStrokeWidth(progressWidth);
+
+    progressSecondPaint = new Paint();
+    progressSecondPaint.setAntiAlias(true);
+    progressSecondPaint.setColor(progressSecondColor);
+    progressSecondPaint.setStyle(Paint.Style.STROKE);
+    progressSecondPaint.setStrokeWidth(progressWidth);
   }
 
   @Override
@@ -71,33 +86,98 @@ public class ProgressView extends View {
     drawProgress(canvas);
   }
 
+  /**
+   * 画圆
+   * @param canvas
+   */
   private void drawCircle(Canvas canvas) {
     xCenter = yCenter = getWidth() / 2;
-    endRadius = getWidth() / 2;
-    startRadius = endRadius - diffRadius;
-    if (isClick && !isFinish) {
-      canvas.drawCircle(xCenter, yCenter, endRadius, cirlePaint);
+    startRadius = getWidth() / 2 - maxDiffRadius;
+    canvas.drawCircle(xCenter, yCenter, startRadius + diffRadius, cirlePaint);
+  }
+
+
+  /**
+   * 画进度条
+   * @param canvas
+   */
+
+  private void drawProgress(Canvas canvas) {
+    if (!progressEnable) {
+      return;
+    }
+    if (isClick) {
+      if (progressOval == null) {
+        progressOval = new RectF(maxDiffRadius, maxDiffRadius, maxDiffRadius + 2 * startRadius,
+            maxDiffRadius + 2 * startRadius);
+      }
+      canvas.drawArc(progressOval, -90, progress, false, progressPaint);
+      canvas.drawArc(progressOval, -90, 360, false, progressSecondPaint);
+      if (progress >= 360) {
+        progress = 0;
+        isClick = false;
+        isFinish = true;
+        startCircleAnimator();
+        if (onFinishListener != null) {
+          onFinishListener.onFinish();
+        }
+      }
     } else {
-      canvas.drawCircle(xCenter, yCenter, startRadius, cirlePaint);
+      progress = 0;
     }
   }
 
-  private void drawProgress(Canvas canvas) {
-    if (isClick && !isFinish) {
-      progressOval = new RectF(diffRadius, diffRadius, diffRadius + 2 * startRadius,
-          diffRadius + 2 * startRadius);
-      canvas.drawArc(progressOval, -90, progress, false, progressPaint);
-      progress = progress + rate;
-      if (progress >= 360) {
-        isFinish = true;
-        progress = 0;
-        if (onFinishListner != null) {
-          onFinishListner.onFinish();
-        }
-      }
-      invalidate();
+  private ValueAnimator circleAnimator = null;
+
+  /**
+   * 画圆的动画  isclick 为true时扩张， 为false时收缩
+   */
+  private void startCircleAnimator() {
+    if (circleAnimator != null) {
+      circleAnimator.cancel();
+    }
+    if (isClick) {
+      circleAnimator = ValueAnimator.ofInt(0, maxDiffRadius);
     } else {
-      progress = 0;
+      circleAnimator = ValueAnimator.ofInt(diffRadius, maxDiffRadius, 0);
+    }
+    circleAnimator.setDuration(500);
+    circleAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        diffRadius = (int) animation.getAnimatedValue();
+        invalidate();
+      }
+    });
+    circleAnimator.start();
+  }
+
+  private ValueAnimator progressAnimator = null;
+
+  /**
+   * 画进度条的动画
+   */
+  public void startProgressAnimator() {
+    if (!progressEnable) {
+      return;
+    }
+    cancleProgressAnimator();
+    progressAnimator = ValueAnimator.ofInt(0, 360);
+    progressAnimator.setDuration(duration);
+    progressAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        progress = (int) animation.getAnimatedValue();
+        invalidate();
+      }
+    });
+    progressAnimator.start();
+  }
+
+  public void cancleProgressAnimator() {
+    if (progressAnimator != null) {
+      progressAnimator.cancel();
     }
   }
 
@@ -106,25 +186,40 @@ public class ProgressView extends View {
     switch (event.getAction()) {
       case MotionEvent.ACTION_DOWN:
         isClick = true;
-        invalidate();
+        startCircleAnimator();
+        startProgressAnimator();
         break;
       case MotionEvent.ACTION_UP:
+        if (!progressEnable && onClickListener != null) {
+          onClickListener.onClick(this);
+        }
       case MotionEvent.ACTION_CANCEL:
         isClick = false;
+        cancleProgressAnimator();
+        if (!isFinish) {
+          startCircleAnimator();
+        }
         isFinish = false;
-        invalidate();
         break;
 
     }
     return true;
   }
 
-  public void setOnFinishListner(OnFinishListner onFinishListner) {
-    this.onFinishListner = onFinishListner;
+  public void setOnFinishListener(OnFinishListener onFinishListner) {
+    this.onFinishListener = onFinishListner;
   }
 
-  public interface OnFinishListner {
+  public void setOnClickListener(OnClickListener onClickListner) {
+    this.onClickListener = onClickListner;
+  }
+
+  public interface OnFinishListener {
     void onFinish();
+  }
+
+  public interface OnClickListener {
+    void onClick(View view);
   }
 
 
